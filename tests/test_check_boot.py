0876fa90fd536bc3da6bda0eb743144000d6ed18
Gonzalo Rafuls <grafuls@redhat.com>
2018-12-06 11:59:07 +0100

    RE-Optimized imports in test suite

--- tests/test_check_boot.py
+++ tests/test_check_boot.py
@@ -1,9 +1,9 @@
-import unittest
 import pytest
 import requests_mock
-import tests.config as conf
+import unittest
 
 from badfish import main
+from tests import config
 
 
 class TestCheckBoot(unittest.TestCase):
@@ -14,37 +14,37 @@ class TestCheckBoot(unittest.TestCase):
 
     @requests_mock.mock()
     def badfish_call(self, _mock):
-        _mock.get("https://%s/redfish/v1/Systems/System.Embedded.1/Bios" % conf.MOCK_HOST,
+        _mock.get("https://%s/redfish/v1/Systems/System.Embedded.1/Bios" % config.MOCK_HOST,
                   json={"Attributes": {"BootMode": u"Bios"}}
                   )
-        _mock.get("https://%s/redfish/v1/Systems/System.Embedded.1/BootSources" % conf.MOCK_HOST,
+        _mock.get("https://%s/redfish/v1/Systems/System.Embedded.1/BootSources" % config.MOCK_HOST,
                   json={"Attributes": {"BootSeq": self.boot_seq}})
-        argv = ["-H", conf.MOCK_HOST, "-u", conf.MOCK_USER, "-p", conf.MOCK_PASS]
+        argv = ["-H", config.MOCK_HOST, "-u", config.MOCK_USER, "-p", config.MOCK_PASS]
         argv.extend(self.args)
         assert not main(argv)
         out, err = self._capsys.readouterr()
         return err
 
     def test_check_boot_without_interfaces(self):
-        self.boot_seq = conf.BOOT_SEQ_RESPONSE_DIRECTOR
+        self.boot_seq = config.BOOT_SEQ_RESPONSE_DIRECTOR
         self.args = ["--check-boot"]
         result = self.badfish_call()
-        assert conf.RESPONSE_WITHOUT == result
+        assert config.RESPONSE_WITHOUT == result
 
     def test_check_boot_with_interfaces_director(self):
-        self.boot_seq = conf.BOOT_SEQ_RESPONSE_DIRECTOR
-        self.args = ["-i", conf.INTERFACES_PATH, "--check-boot"]
+        self.boot_seq = config.BOOT_SEQ_RESPONSE_DIRECTOR
+        self.args = ["-i", config.INTERFACES_PATH, "--check-boot"]
         result = self.badfish_call()
-        assert conf.RESPONSE_DIRECTOR == result
+        assert config.RESPONSE_DIRECTOR == result
 
     def test_check_boot_with_interfaces_foreman(self):
-        self.boot_seq = conf.BOOT_SEQ_RESPONSE_FOREMAN
-        self.args = ["-i", conf.INTERFACES_PATH, "--check-boot"]
+        self.boot_seq = config.BOOT_SEQ_RESPONSE_FOREMAN
+        self.args = ["-i", config.INTERFACES_PATH, "--check-boot"]
         result = self.badfish_call()
-        assert conf.RESPONSE_FOREMAN == result
+        assert config.RESPONSE_FOREMAN == result
 
     def test_check_boot_no_match(self):
-        self.boot_seq = conf.BOOT_SEQ_RESPONSE_NO_MATCH
-        self.args = ["-i", conf.INTERFACES_PATH, "--check-boot"]
+        self.boot_seq = config.BOOT_SEQ_RESPONSE_NO_MATCH
+        self.args = ["-i", config.INTERFACES_PATH, "--check-boot"]
         result = self.badfish_call()
-        assert conf.WARN_NO_MATCH == result
+        assert config.WARN_NO_MATCH == result
