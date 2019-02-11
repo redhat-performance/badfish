f9cbfd7f491ffc72b185d864e85a3297e0fe954d
Gonzalo Rafuls <grafuls@redhat.com>
2019-02-11 11:17:19 +0100

    Removed print statement from tests

--- tests/test_change_boot.py
+++ tests/test_change_boot.py
@@ -11,21 +11,18 @@ class TestChangeBoot(TestBase):
         self.boot_seq = config.BOOT_SEQ_RESPONSE_DIRECTOR
         self.args = ["-i", config.INTERFACES_PATH, self.option_arg, "foreman"]
         _, err = self.badfish_call()
-        print("!!!! %s !!!!" % err)
         assert config.RESPONSE_CHANGE_BOOT == err
 
     def test_change_to_director(self):
         self.boot_seq = config.BOOT_SEQ_RESPONSE_FOREMAN
         self.args = ["-i", config.INTERFACES_PATH, self.option_arg, "director"]
         _, err = self.badfish_call()
-        print("!!!! %s !!!!" % err)
         assert config.RESPONSE_CHANGE_BOOT == err
 
     def test_change_to_same(self):
         self.boot_seq = config.BOOT_SEQ_RESPONSE_DIRECTOR
         self.args = ["-i", config.INTERFACES_PATH, self.option_arg, "director"]
         _, err = self.badfish_call()
-        print("!!!! %s !!!!" % err)
         assert config.RESPONSE_CHANGE_NO_INT == err
 
     def test_change_no_interfaces(self):
