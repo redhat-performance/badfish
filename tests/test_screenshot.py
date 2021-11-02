3a5349844d53c0d78491a83b02f12cc7d115c9c5
Gonza Rafuls <grafuls@redhat.com>
2021-11-02 12:26:17 +0100

    tests: added test for screenshots

---
+++ tests/test_screenshot.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+
+from asynctest import patch
+from tests.config import (
+    INIT_RESP,
+    SCREENSHOT_RESP,
+    SCREENSHOT_OK,
+    SCREENSHOT_NAME,
+)
+from tests.test_base import TestBase
+
+
+class TestScreenshot(TestBase):
+    option_arg = "--screenshot"
+
+    @pytest.fixture(scope="function", autouse=True)
+    def teardown(self, request):
+        def remove_file():
+            os.remove(SCREENSHOT_NAME)
+        request.addfinalizer(remove_file)
+
+    @patch("aiohttp.ClientSession.post")
+    @patch("aiohttp.ClientSession.get")
+    def test_screenshot_ok(self, mock_get, mock_post):
+
+        self.set_mock_response(mock_get, 200, INIT_RESP)
+        self.set_mock_response(mock_post, 200, SCREENSHOT_RESP)
+        self.args = [self.option_arg]
+
+        with patch("time.strftime", return_value="now"):
+            _, err = self.badfish_call()
+
+        assert err == SCREENSHOT_OK
