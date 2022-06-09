763338219cbf268dee9953a9041c75fbae48c946
Gonza Rafuls <grafuls@redhat.com>
2022-06-09 14:41:23 +0200

    fix: package rpm installation

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    install_requires=[
-        "pyyaml>=3.10",
-        "aiohttp>=3.7.4",
-        "setuptools>=42.0",
-        "pillow>=5.1.0",
-    ],
-    entry_points={"console_scripts": ["badfish=badfish.badfish:main"]},
-)
+setuptools.setup()
